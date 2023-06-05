b1eaee24deac26a5ffd2bc3e1351b6632fc8b130
phi <phungvietphi88@gmail.com>
2023-06-06 00:19:03 +0700

    Sua Router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 const app = express();
+import userRouter from "./routers/user"
+import authorRouter from "./routers/author"
+import comicRouter from "./routers/comic"
+import categoryRouter from "./routers/category"
 
 app.use(express.json())
 
 //routes
+app.use("/api", categoryRouter);
+app.use("/api", comicRouter);
+app.use("/api", authorRouter);
+app.use("/api", userRouter);
 
 mongoose.connect("mongodb://localhost:27017/")
 export const viteNodeApp = app
