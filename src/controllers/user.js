617f58cdd73ff93d6eccbd1ce26621666222c6a7
phi <phungvietphi88@gmail.com>
2023-06-06 00:56:08 +0700

    feat:Controller User  refs: G2-6

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,22 +2,77 @@ import User from "../models/user";
 import bcrypt from "bcryptjs";
 import { signinSchema, signupSchema } from "../schemas/user";
 import jwt from "jsonwebtoken"
+
 export const signUp = async (req, res) => {
     try {
-        // Code
-    } catch (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
-}
+
+        const { error } = signupSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+
+            return res.status(400).json({
+                messages: errors,
+            });
+        }
+
+        const userExist = await User.findOne({ email: req.body.email });
+        if (userExist) {
+            return res.status(400).json({
+                messages: "Email đã tồn tại",
+            });
+        }
+
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user = await User.create({
+            ...req.body,
+            password: hashedPassword,
+        });
+
+        const token = jwt.sign({ id: user._id }, { expiresIn: "1d" });
+        user.password = undefined;
+        return res.status(201).json({
+            message: "Tạo tài khoản thành công",
+            accessToken: token,
+            user,
+        });
+    } catch (error) {}
+};
+
 
 export const signIn = async (req, res) => {
+    console.log(1);
     try {
-        // Code
-    } catch (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
-}
+        const { error } = signinSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+
+            return res.status(400).json({
+                messages: errors,
+            });
+        }
+
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({
+                messages: "Email không tồn tại",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                messages: "Sai mật khẩu",
+            });
+        }
+        const token = jwt.sign({ id: user._id }, "abc", { expiresIn: "1d" });
+        user.password = undefined;
+        return res.status(200).json({
+            message: "Đăng nhập thành công",
+            accessToken: token,
+            user,
+        });
+    } catch (error) {}
+};
+
