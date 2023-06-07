c05f2ec49ec25a4f498d27f32e59f6a82c6fae62
[ Thuy Milion ] <106894317+HongThuy203@users.noreply.github.com>
2023-06-07 22:42:16 +0700

    feat: create-models repair comic model refs: G2-1

--- src/models/comic.js
+++ src/models/comic.js
@@ -4,8 +4,8 @@ const comicSchema = new mongoose.Schema({
     name: {
         type: String,
     },
-    price: {
-        type: Number,
+    description: {
+        type: String,
     },
     image: {
         type: String,
