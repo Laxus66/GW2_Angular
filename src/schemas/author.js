8d021a0886c41392a5fecc8272f72ba868e6ec46
Laxus66 <Laxus2301@gmail.com>
2023-06-12 02:33:05 +0700

    feat: Update Schema All refs: G2-4

--- src/schemas/author.js
+++ src/schemas/author.js
@@ -1,9 +1,5 @@
 import joi from "joi";
 export const authorSchema = joi.object({
-    id: joi.number().required().messages({
-        "number.base": "ID phải là một số",
-        "any.required": "ID là bắt buộc",
-    }),
     name: joi.string().required().messages({
         "string.empty": "Tên không được để trống",
         "any.required": "Tên là bắt buộc",
