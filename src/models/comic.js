9ae91fa71d91000ebdcad262e5c6918b59a7b293
tiendkph20777 <tiendkph20777@fpt.edu.vn>
2023-06-13 04:22:37 +0700

    update-comic

--- src/models/comic.js
+++ src/models/comic.js
@@ -1,7 +1,44 @@
-import mongoose from "mongose"
+import mongoose from "mongoose"
+
 const comicSchema = new mongoose.Schema({
-    // Code
-},
-    { timestamps: true, versionKey: false }
-)
+    name: {
+        type: String,
+        required: [true, "Tên là bắt buộc"],
+    },
+    author_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Author",
+        required: [true, "ID tác giả là bắt buộc"],
+    },
+    images: {
+        // type: [
+        //     {
+        //         id: {
+        //             type: Number,
+        //             required: [true, "ID hình ảnh là bắt buộc"],
+        //         },
+        //         image: {
+        //             type: String,
+        //             required: [true, "Hình ảnh là bắt buộc"],
+        //         },
+        //     },
+        // ],
+        type: String,
+        required: [true, "Danh sách hình ảnh là bắt buộc"],
+    },
+    story: {
+        type: String,
+        required: [true, "Story là bắt buộc"],
+    },
+    description: {
+        type: String,
+        required: [true, "Mô tả là bắt buộc"],
+    },
+    category_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Category",
+        required: [true, "ID danh mục là bắt buộc"],
+    },
+}, { timestamps: true, versionKey: false });
+
 export default mongoose.model("Comic", comicSchema);
--- src/schemas/comic.js
+++ src/schemas/comic.js
@@ -1,30 +1,39 @@
+import joi from "joi";
 export const comicSchema = joi.object({
     name: joi.string().required().messages({
         "string.empty": "Tên không được để trống",
         "any.required": "Tên là bắt buộc",
     }),
-    author_id: joi.number().required().messages({
+    author_id: joi.string().required().messages({
         "number.base": "ID tác giả phải là một số",
         "any.required": "ID tác giả là bắt buộc",
     }),
-    images: joi.array().items(joi.object({
-        id: joi.number().required().messages({
-            "number.base": "ID hình ảnh phải là một số",
-            "any.required": "ID hình ảnh là bắt buộc",
-        }),
-        image: joi.string().required().messages({
-            "string.empty": "Hình ảnh không được để trống",
-            "any.required": "Hình ảnh là bắt buộc",
-        }),
-    })).required().messages({
-        "array.base": "Danh sách hình ảnh phải là một mảng",
-        "any.required": "Danh sách hình ảnh là bắt buộc",
+    images: joi.string().required().messages({
+        "string.empty": "image không được để trống",
+        "any.required": "image là bắt buộc",
+    }),
+    // images: joi.array().items(joi.object({
+    //     id: joi.number().required().messages({
+    //         "number.base": "ID hình ảnh phải là một số",
+    //         "any.required": "ID hình ảnh là bắt buộc",
+    //     }),
+    //     image: joi.string().required().messages({
+    //         "string.empty": "Hình ảnh không được để trống",
+    //         "any.required": "Hình ảnh là bắt buộc",
+    //     }),
+    // })).required().messages({
+    //     "array.base": "Danh sách hình ảnh phải là một mảng",
+    //     "any.required": "Danh sách hình ảnh là bắt buộc",
+    // }),
+    story: joi.string().required().messages({
+        "string.empty": "story không được để trống",
+        "any.required": "story là bắt buộc",
     }),
     description: joi.string().required().messages({
         "string.empty": "Mô tả không được để trống",
         "any.required": "Mô tả là bắt buộc",
     }),
-    cate_id: joi.number().required().messages({
+    category_id: joi.string().required().messages({
         "number.base": "ID danh mục phải là một số",
         "any.required": "ID danh mục là bắt buộc",
     }),
